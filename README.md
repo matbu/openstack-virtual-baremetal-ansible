675e08c6532464aba61159d771b8acb3e8476809
Mathieu Bultel <mbultel@redhat.com>
2015-12-14 14:01:54 +0100

    Add script shell for running ansible and Readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Openstack Virtual Baremetal - Ansible
+---------------------------------------
+
+This project contain some Ansible playbooks to be able to deploy Openstack
+Virtual Baremetal on top of devstack.
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+if [ ! -d ansible_venv ]; then
+    virtualenv ansible_venv
+fi
+source ansible_venv/bin/activate
+pip install -U ansible==1.9.2
+pip install markupsafe
+
+set +e
+anscmd="stdbuf -oL -eL ansible-playbook -vvvv"
+echo "Deploy devstack"
+$anscmd -i hosts devstack/main.yml
+echo "Deploy OVB"
+$anscmd -i hosts ovb/main.yml
